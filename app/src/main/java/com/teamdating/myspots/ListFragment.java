7bf34e331729241a8d09a8ca1547da4834801f00
Janine Boeser <janine@boeser.top>
2017-03-23 16:25:08 +0100

    Fragments

--- app/src/main/java/com/teamdating/myspots/ListFragment.java
+++ app/src/main/java/com/teamdating/myspots/ListFragment.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
@@ -18,20 +19,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import java.util.List;
+
 public class ListFragment extends Fragment {
 
     private RecyclerView mRecyclerView;
+    private SpotsDataSource mDataSource;
+    private SpotAdapter mAdapter;
+    private List<SpotItem> mItems;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,@Nullable ViewGroup container,@Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_list, container, false);
-        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+
+        View view = inflater.inflate(R.layout.activity_list_row, container, false);
         RecyclerView mRecyclerView = (RecyclerView) view.findViewById(R.id.spots_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        mRecyclerView.setAdapter(new SpotAdapter());
+        mRecyclerView.setAdapter(new SpotAdapter(SpotAdapter.MyViewHolder.populateRow(mItems), R.layout.activity_list_row));
 
+        SpotItem spotItemList[] = {
+                new SpotItem("Zara", "Haarlem", 52.378757, 4.632956)
+        };
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/teamdating/myspots/SpotAdapter.java
+++ app/src/main/java/com/teamdating/myspots/SpotAdapter.java
@@ -3,40 +3,84 @@ package com.teamdating.myspots;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.CursorWrapper;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 
+import java.util.List;
+
+import static android.support.v7.widget.RecyclerView.*;
+
 /**
  * Created by j.boeser on 16-3-2017.
  */
 
-public class SpotAdapter extends CursorAdapter {
+public class SpotAdapter extends RecyclerView.Adapter<SpotAdapter.MyViewHolder> {
 
-    public SpotAdapter (Context context, Cursor c) {
-        super(context, c, 0);
+    final Context context;
+    private final List<SpotItem> spotItemList;
+
+    public SpotAdapter(List<SpotItem> spotItemList, Context context) {
+        this.spotItemList = spotItemList;
+        this.context = context;
     }
 
     @Override
-    public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        LayoutInflater inflater = LayoutInflater.from(context);
-        return inflater.inflate(R.layout.activity_list_row, parent, false);
+    public int getItemCount() {
+        return spotItemList.size();
+    }
+
+    private SpotItem getItem(int position) {
+        return spotItemList.get(position);
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-        PlaceCursorWrapper cursorWrapper = (PlaceCursorWrapper) cursor;
-        SpotItem hotspot = cursorWrapper.getPlace();
+    public long getItemId(int position) {
+        return spotItemList.get(position).getId();
+    }
 
-        TextView name = (TextView) view.findViewById(R.id.name);
-        name.setText(hotspot.getName());
-        TextView city = (TextView) view.findViewById(R.id.city);
-        city.setText(hotspot.getCity());
-        TextView longitude = (TextView) view.findViewById(R.id.longitude);
-        longitude.setText((int) hotspot.getLongitude());
-        TextView latitude = (TextView) view.findViewById(R.id.latitude);
-        latitude.setText((int) hotspot.getLatitude());
+    public void updateList(List<SpotItem> newList) {
+        spotItemList.clear();
+        spotItemList.addAll(newList);
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_list_row, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(SpotAdapter.MyViewHolder holder, int position) {
+        holder.populateRow(getItem(position));
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        protected TextView vName;
+        protected TextView vCity;
+        protected TextView vLongitude;
+        protected TextView vLatitude;
+
+        public MyViewHolder (View v) {
+            super(v);
+            vName = (TextView) v.findViewById(R.id.name);
+            vCity = (TextView) v.findViewById(R.id.city);
+            vLongitude = (TextView) v.findViewById(R.id.longitude);
+            vLatitude = (TextView) v.findViewById(R.id.latitude);
+
+            v.setOnClickListener((OnClickListener) this);
+        }
+
+        public void populateRow (SpotItem spotItem) {
+            vName.setText(spotItem.getName());
+            vCity.setText(spotItem.getCity());
+            vLongitude.setText((int) spotItem.getLongitude());
+            vLatitude.setText((int) spotItem.getLatitude());
+        }
     }
 }
